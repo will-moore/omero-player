97f7825297361cb05e8a64198f5e5f4b7d4da2e7
William Moore <w.moore@dundee.ac.uk>
2016-08-21 23:56:32 +0100

    Remove 'react-html-attrs' and 'transform-decorators-legacy' plugins
    Also did  npm install babel-plugin-transform-class-properties since this was missing

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+          plugins: ['transform-class-properties'],
         }
       }
     ]
