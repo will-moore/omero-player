b7ae66ca1dcc846e7d516e99ba96f5fa069ce1b5
William Moore <w.moore@dundee.ac.uk>
2016-08-28 00:02:18 +0100

    Started using react-bootstrap

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+// import { Button } from 'react-bootstrap';
+import PlayerNav from './PlayerNav';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, Sarah</h1>
+        <div>
+            <PlayerNav></PlayerNav>
+            <div>Content</div>
+        </div>
     );
   }
 }
